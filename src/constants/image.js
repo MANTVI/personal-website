13c4a4e7f8fd4c541e21c24b6285e077d40ae3b4
MANTVI <suresh.sc123.123@gmail.comI>
2024-07-06 00:06:57 +0530

    Required images

---
+++ src/constants/image.js
@@ -0,0 +1,55 @@
+import upworkIcon from "../assets/upwork.svg";
+import emailIcon from "../assets/email.svg";
+import githubIcon from "../assets/github.svg";
+import codepenIcon from "../assets/codepen.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+import htmlLogo from "../assets/logo/html-logo.svg";
+import cssLogo from "../assets/logo/css-logo.svg";
+import javascriptLogo from "../assets/logo/javascript-logo.svg";
+import reactLogo from "../assets/logo/react-logo.svg";
+import pythonLogo from "../assets/logo/Python-logo.png";
+import jupyter from "../assets/logo/jupyter-logo.png";
+import tailwindLogo from "../assets/logo/tailwind-logo.svg";
+import gitLogo from "../assets/logo/git-logo.svg";
+import githubLogo from "../assets/logo/github-logo.svg";
+import nextLogo from "../assets/logo/next-logo.svg";
+import hubspotLogo from "../assets/logo/hubspot-logo.svg";
+// import felixImage from "../assets/whacky-felix.png";
+import vimal from "../assets/vimal.jpg";
+import errorIllustration from "../assets/illustrations/404-vector.svg";
+import successIllustration from "../assets/illustrations/success-vector.svg";
+import dark from "../assets/icons/dark.png";
+import light from "../assets/icons/light.png";
+import themeswitch from "../assets/projects/themeswitcher.png";
+import Blog from "../assets/projects/BlogWebsite.png";
+import yourlogo from "../assets/projects/website.png";
+import analysis from "../assets/projects/analysis.png";
+
+export default {
+  vimal,
+  jupyter,
+  analysis,
+  yourlogo ,
+  pythonLogo,
+  dark,
+  light,
+  Blog,
+  themeswitch,
+  emailIcon,
+  upworkIcon,
+  githubIcon,
+  codepenIcon,
+  linkedinIcon,
+  htmlLogo,
+  cssLogo,
+  javascriptLogo,
+  reactLogo,
+  tailwindLogo,
+  gitLogo,
+  githubLogo,
+  nextLogo,
+  hubspotLogo,
+  // felixImage,
+  errorIllustration,
+  successIllustration,
+};
